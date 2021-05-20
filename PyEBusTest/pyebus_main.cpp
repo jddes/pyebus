aa283167ed406d936b0dcb7c44d92bde4fa0b3f1
jddes <jean-daniel.deschenes@gel.ulaval.ca>
2021-05-19 21:10:51 -0400

    Rename to pyebustest

--- PyEBusTest/pyebus_main.cpp
+++ PyEBusTest/pyebus_main.cpp
@@ -1,24 +1,13 @@
 #include <Python.h>
 #include <Windows.h>
-#include <cmath>
-
-const double e = 2.7182818284590452353602874713527;
-
-double sinh_impl(double x) {
-    return (1 - pow(e, (-2 * x))) / (2 * pow(e, -x));
-}
-
-double cosh_impl(double x) {
-    return (1 + pow(e, (-2 * x))) / (2 * pow(e, -x));
-}
 
 PyObject* tanh_impl(PyObject* /* unused module reference */, PyObject* o) {
     double x = PyFloat_AsDouble(o);
-    double tanh_x = sinh_impl(x) / cosh_impl(x);
+    double tanh_x = x;
     return PyFloat_FromDouble(tanh_x);
 }
 
-static PyMethodDef superfastcode_methods[] = {
+static PyMethodDef pyebustest_methods[] = {
     // The first property is the name exposed to Python, fast_tanh
     // The second is the C++ function with the implementation
     // METH_O means it takes a single PyObject argument
@@ -28,14 +17,14 @@ static PyMethodDef superfastcode_methods[] = {
     { nullptr, nullptr, 0, nullptr }
 };
 
-static PyModuleDef superfastcode_module = {
+static PyModuleDef pyebustest_module = {
     PyModuleDef_HEAD_INIT,
-    "superfastcode",                        // Module name to use with Python import statements
+    "pyebustest",                        // Module name to use with Python import statements
     "Provides some functions, but faster",  // Module description
     0,
-    superfastcode_methods                   // Structure that defines the methods of the module
+    pyebustest_methods                   // Structure that defines the methods of the module
 };
 
-PyMODINIT_FUNC PyInit_superfastcode() {
-    return PyModule_Create(&superfastcode_module);
+PyMODINIT_FUNC PyInit_pyebustest() {
+    return PyModule_Create(&pyebustest_module);
 }
